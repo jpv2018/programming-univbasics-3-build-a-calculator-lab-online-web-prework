c0fd2ef30c38830b2fe5fcc1f925a14c928556b9
Jason Viola <jason.viola@mavs.uta.edu>
2019-06-28 03:23:58 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,34 @@
 def addition(num1, num2)
-
+x = "#{num1}".to_i
+y = "#{num2}".to_i
+x+y
 end
 
 def subtraction(num1, num2)
-
+x = "#{num1}".to_i
+y = "#{num2}".to_i
+x-y
 end
 
 def division(num1, num2)
-
+x = "#{num1}".to_i
+y = "#{num2}".to_i
+x/y
 end
 
 def multiplication(num1, num2)
-
+x = "#{num1}".to_i
+y = "#{num2}".to_i
+x*y
 end
 
 def modulo(num1, num2)
-
+x = "#{num1}".to_i
+y = "#{num2}".to_i
+x%y
 end
 
 def square_root(num)
-
+x = "#{num}".to_i
+Math.sqrt x
 end
