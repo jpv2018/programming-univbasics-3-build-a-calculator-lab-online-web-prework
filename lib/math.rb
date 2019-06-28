b9fe43872b1607c4910b4016808f633651d82b7d
Jason Viola <jason.viola@mavs.uta.edu>
2019-06-28 03:17:44 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -11,11 +11,15 @@ x-y
 end
 
 def division(num1, num2)
-p "#{num1} / #{num2}"
+x = "#{num1}".to_i
+y = "#{num2}".to_i
+x/y
 end
 
 def multiplication(num1, num2)
-p "#{num1} * #{num2}"
+x = "#{num1}".to_i
+y = "#{num2}".to_i
+x*y
 end
 
 def modulo(num1, num2)
@@ -23,5 +27,5 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-p "math.sqrt#{num}"
+math.sqrt("#{num}".to_i)
 end
